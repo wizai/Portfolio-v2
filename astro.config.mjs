5d462f799428affc66f4456d4b902e88577380b6
wizai <william.nisole@gmail.com>
2026-09-07 19:18:50 +0200

    fix: 🐛 SEO trading slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://wizai.fr',
+    trailingSlash: 'never',
     integrations: [sitemap()],
     vite: {
     plugins: [tailwindcss()],
